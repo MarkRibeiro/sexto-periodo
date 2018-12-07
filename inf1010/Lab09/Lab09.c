c241e451531315e23d426f339e5caf2b58991155
Mark <markpribeiro@hotmail.com>
2018-12-07 18:02:31 -0200

    Mark

--- inf1010/Lab09/Lab09.c
+++ inf1010/Lab09/Lab09.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+#include<stdbool.h>
+#define MAX_VERTICES 7
+int grafo[MAX_VERTICES][MAX_VERTICES];
+int visited[MAX_VERTICES];
+
 typedef struct no
 {
 	int chave[2];
@@ -41,14 +46,61 @@ void exibechave(int lim_in, int lim_sup, No *arv)
 		exibechave(lim_in, lim_sup, arv->maior);
 }
 
+void dfs(int v) 
+{
+	int w;
+
+	printf("%3d", v);
+	visited[v] = 1;
+	for (w = 0; w < MAX_VERTICES; w++)
+	if (grafo[v][w] && !visited[w]) dfs(w);
+}
+
+void imprimebsf(int i)
+{
+	int cont=0, aux=0, x=0;
+	int j, lista[5];
+	while(cont<MAX_VERTICES)
+	{
+		printf("%d\n", i);
+		visited[i]=true;
+		for(j=0;j<MAX_VERTICES;j++)
+		{
+			if(grafo[i][j] && !visited[j])
+			{
+				lista[aux]=j;
+				aux=(aux+1)%5;
+			}
+		}
+		i = lista[x];
+		x = (x +1)%5;
+		cont++;
+	}
+}
+
 int main (void)
 {
 	No *arv; 
 	int n, ret, lim_in, lim_sup;
+	grafo[0][5]=true;
+	grafo[0][1]=true;
+	grafo[5][0]=true;
+	grafo[5][4]=true;
+	grafo[4][5]=true;
+	grafo[4][6]=true;
+	grafo[6][4]=true;
+	grafo[6][3]=true;
+	grafo[6][1]=true;
+	grafo[3][6]=true;
+	grafo[1][6]=true;
+	grafo[1][0]=true;
+	grafo[1][2]=true;
+	grafo[2][1]=true;
 
+	//ret=buscaNaArvB(n, arv);
 
-	ret=buscaNaArvB(n, arv);
-
+	imprimebsf(0);
+/*
 	switch (ret)
 	{
 		case 0:
@@ -59,6 +111,6 @@ int main (void)
 	}
 
 	exibechave(lim_in, lim_sup, arv);
-
+	*/
 	return 0;
 }
